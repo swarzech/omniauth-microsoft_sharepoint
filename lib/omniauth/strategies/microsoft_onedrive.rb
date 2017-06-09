59820f8ed3a8cec301a697e84339795d8f44af4e
Tommy Bonderenka <machine.valley@gmail.com>
2017-06-09 09:35:47 -0500

    test removing params

--- lib/omniauth/strategies/microsoft_onedrive.rb
+++ lib/omniauth/strategies/microsoft_onedrive.rb
@@ -20,13 +20,13 @@ module OmniAuth
         super
       end
 
-      option :authorize_params, {
-        resource: 'https://graph.microsoft.com/'
-      }
-
-      option :token_params, {
-        resource: 'https://graph.microsoft.com/'        
-      }
+      # option :authorize_params, {
+      #   resource: 'https://graph.microsoft.com/'
+      # }
+      # 
+      # option :token_params, {
+      #   resource: 'https://graph.microsoft.com/'        
+      # }
 
       uid { raw_info["id"] }
 
